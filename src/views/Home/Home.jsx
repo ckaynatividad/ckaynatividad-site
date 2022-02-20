7820b82c727c66dcef198f7605f2cc7ad83a5f31
ckaynatividad <natividad.ckay@gmail.com>
2022-02-19 21:39:48 -0800

    styling fixes for home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -28,24 +28,32 @@ export default function Home() {
       </header>
       <div className="Home">
         <h1>Hi, I'm Ckay. </h1>
-        <p className="chime">
-          <img className="line" src={require('.//line2.png')} alt="" />{' '}
+        <p className="top-line">
+          <img className="line" src={require('./assets/line2.png')} />
         </p>
-        <img className="line2" src={require('.//line2.png')} alt="" />
-        <img className="pic" src={require('.//pic.jpg')} alt="" />
+        <img className="line2" src={require('./assets/line2.png')} />
+        <img className="pic" src={require('./assets/pic.jpg')} />
         <div className="description">
           <p className="text">
             Full stack web developer with a knack for user interface design. I
             also like art and flowers.
             <p className="city">Based in Portland, Oregon.</p>
-            <div className="icon-pic">
+            <span className="icon-pic">
               <a href="https://www.linkedin.com/in/ckaynatividad/">
-                <img className="icon" src={require('.//insta.png')} alt="" />
+                <img
+                  className="icon"
+                  src={require('./assets/linked.png')}
+                  alt=""
+                />
               </a>
               <a href="https://github.com/ckaynatividad">
-                <img className="icon2" src={require('.//github.png')} alt="" />
+                <img
+                  className="icon2"
+                  src={require('./assets/github.png')}
+                  alt=""
+                />
               </a>
-            </div>
+            </span>
           </p>
         </div>
       </div>
