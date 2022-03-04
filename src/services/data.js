ecba65bb92f8c179d3796ece5cf18f96fc0aa460
ckaynatividad <natividad.ckay@gmail.com>
2022-03-03 19:15:46 -0800

    added new project

--- src/services/data.js
+++ src/services/data.js
@@ -1,5 +1,7 @@
 export const art = [{}];
 
+import foto from '../views/Home/assets/foto.png';
+
 export const appsData = [
   {
     id: 1,
@@ -11,11 +13,11 @@ export const appsData = [
   },
   {
     id: 2,
-    title: 'Insert Title',
-    desc: 'Insert Desc',
-    img: 'https://64.media.tumblr.com/ac85967f90b02d3a81dc05a38df8bf03/8d647e8b8d745ee0-0e/s1280x1920/0528f106498ba775ec10838f412bdc62ce3e650b.png',
-    url: 'Insert URL',
-    date: 'Date',
+    title: 'Foto.',
+    desc: 'Foto. is an application that allows users to upload photos public or private albums. Public album photos are randomized across the home page, while private albums are password protected.',
+    img: 'https://64.media.tumblr.com/bdd31a792e1b589a53300b1240339f94/164f9d83b5527a63-c2/s2048x3072/bf93dd8e9213ae356d16133c399cb3fb6af0953c.png',
+    url: 'https://github.com/photo-app-placeholder/Foto',
+    date: 'March 2022',
   },
   {
     id: 3,
